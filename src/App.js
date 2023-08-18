c51114b0f74f092a14d0663b86c03614f66fb246
Divyshekhar <sinha.divyshekhar2004@gmail.com>
2023-08-18 08:30:22 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import ParticlesBg from 'particles-bg'
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event);
+  }
+  onButtonSubmit = () =>{
+    console.log("click");
+  }
+  render(){
+  return (
+    <div className="App">
+     <ParticlesBg className='particles' type="cobweb" color='' bg={true} interactivity= 'true' in={true} />
+     <Navigation />
+     <Logo />
+     <Rank />
+     <ImageLinkForm onInputChange = {this.onInputChange} 
+        onButtonSubmit = {this.onButtonSubmit}/>
+     {/* <FaceRecognition />  */}
+    </div>
+  );
+}
+}
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='pa3 link underline black pointer dim'>
+                Sign Out
+            </p>
+        </nav>
+    );
+}
+export default Navigation

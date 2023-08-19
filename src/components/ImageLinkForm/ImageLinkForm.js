8575f782808ba571d5470a3cfdb66059f6ca36eb
Divyshekhar <sinha.divyshekhar2004@gmail.com>
2023-08-20 01:27:08 +0530

    indentation

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
-const ImageLinkForm= ({onInputChange ,onButtonSubmit}) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
-        <p className='f3'>
-            {'This Magic Brain will detect faces in your pictures. Give it a try.'}
-        </p>
+            <p className='f3'>
+                {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+            </p>
             <div className='center'>
-                <div className= 'center form pa4 br3 shadow-5'>
-                <input className='center f3 pa2 w-70' type= "" onChange={onInputChange}/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick= {onButtonSubmit}>
-                    Detect</button>
+                <div className='center form pa4 br3 shadow-5'>
+                    <input className='center f3 pa2 w-70' type="" onChange={onInputChange} />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>
+                        Detect</button>
                 </div>
             </div>
         </div>
